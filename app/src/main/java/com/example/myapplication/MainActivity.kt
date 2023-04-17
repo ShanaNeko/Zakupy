7783ed454fcec12e4106329a915967a298d8a7ae
Shana <Pati.k.06@wp.pl>
2023-04-17 03:19:40 +0200

    'w'

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.annotation.SuppressLint
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
+import java.util.*
 
 //1. Napisz program, który będzie miał dwa tryby; dodwanie do listy zakupów oraz odhaczania zakupów (przełącznik Radiobutton)
 //2. Po wybraniu dodwania widoczne stają się Checkbox'y z 10 dowolnymi zakupami (w 3 grupach np.; warzywa wędliny, pieczywo)
@@ -11,8 +15,45 @@ import android.os.Bundle
 //6.Ocenie podlega również ilość commit'ów.
 
 class MainActivity : AppCompatActivity() {
+    private val shoppingItems = mapOf(
+        "Ciastka" to listOf("Owsiane"),
+
+        "Kwiaty" to listOf("Róże"),
+
+        "Herbaty" to listOf("Earl Grey")
+    )
+    private val selectedItems = mutableListOf<String>()
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listaciastek = findViewById<ChipGroup>(R.id.listaciastek)
+        val listakwiatow = findViewById<ChipGroup>(R.id.listakwiatow)
+        val listaherbat = findViewById<ChipGroup>(R.id.listaherbat)
+        val listazakupow = shoppingItems.values.flatten()
+
+        for (item in listazakupow) {
+            val checkbox = Chip(this).apply {
+                text = item
+                isCheckable = true
+                tag = shoppingItems.entries.firstOrNull { it.value.contains(item) }?.key
+                setOnCheckedChangeListener { _, isChecked ->
+                    if (isChecked) {
+                        selectedItems.add(item)
+                    } else {
+                        selectedItems.remove(item)
+                    }
+                }
+            }
+            checkbox.chipBackgroundColor = resources.getColorStateList(R.color.purple_200)
+
+            when (checkbox.tag) {
+                "Ciastka" -> listaciastek.addView(checkbox)
+                "Kwiaty" -> listakwiatow.addView(checkbox)
+                "Herbaty" -> listaherbat.addView(checkbox)
+            }
+        }
     }
 }
